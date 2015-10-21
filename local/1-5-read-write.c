78d7716c0450ea9b1c60d89d41d8a40ebf4c19e6
Iqbal Talaat Bhatti <iqbaltalaat@gmail.com>
2015-10-21 13:45:02 +1100

    Adding read write file

---
+++ local/1-5-read-write.c
@@ -0,0 +1,24 @@
+#include "apue.h"
+#define BUFFSIZE 4096
+
+int
+main(void)
+{
+    int n;
+    char buf[BUFFSIZE];
+    
+    int keep_reading = 1;
+    while (keep_reading){
+        if (n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0{
+            // File Read Success
+            // Write to output
+            if (write(STDOUT_FILENO, buf, n) != n){
+                err_sys("write error");
+            }
+        }
+        else{
+            keep_reading = 0;
+        }
+    }
+    exit(0);
+}
